2f23b6918d3b13360a72c9250c8e1f0fd3d0aabe
LuXin <2422182563@qq.com>
2026-02-08 20:02:26 +0800

    update

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -1,9 +1,7 @@
 """Collection of the core mathematical operators used throughout the code base."""
 
-import math
 
 # ## Task 0.1
-from typing import Callable, Iterable
 
 #
 # Implementation of a prelude of elementary functions.
